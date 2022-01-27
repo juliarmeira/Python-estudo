f3b7ee5a7b95cf71760e01ced3b28c78b5457b9d
Julia Reis Meira <92880264+juliarmeira@users.noreply.github.com>
2022-01-27 17:23:28 -0300

    Exercicío 3 E 4

---
+++ # Exercicío 3, 4 #.py
@@ -0,0 +1,15 @@
+# Exercicío 3 - PEÇA AO USUÁRIO QUE DIGITE 3 NUMEROS E IMPRIMA A SOMA #
+
+num_1 = input("Digite o primeiro numero")
+num_2 = input("Digite o segundo numero")
+num_3 = input("Digite o terceiro numero")
+
+soma = float(num_1) + float(num_2) + float(num_3)
+
+print("O resultado da soma é %s" %soma)
+
+# Exercicío 4 - PEÇA AO USUÁRIO QUE DIGITE 1 NUMERO E IMPRIMA A QUINTA PARTE #
+num = input("Digite o  numero")
+parte = float(num)/5
+print(parte)
+
