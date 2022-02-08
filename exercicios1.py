6d86770eca6563d48d0ddeb666c72c33d9017acb
Julia Reis Meira <92880264+juliarmeira@users.noreply.github.com>
2022-02-07 21:51:30 -0300

    Mais exercicios

---
+++ exercicios1.py
@@ -0,0 +1,12 @@
+import math
+num_1 = int(input("Digite o primeiro numero:"))
+num_2= int(input("Digite o segundo numero:"))
+if num_1 > num_2:
+    print( " %s é maior" %num_1)
+else:
+    print('%s 2 é maior'%num_2)
+
+if num_1 > 0:
+    raiz = math.sqrt(num_1)
+    print("A raiz quadrada é:", raiz)
+
